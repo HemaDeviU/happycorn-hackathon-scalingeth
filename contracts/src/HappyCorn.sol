9e1a471a46c2278e513b1e2bee97d0b126b75777
Hema Devi <uhemadevi1927@gmail.com>
2024-04-16 20:46:17 +0530

    createitem contract

--- contracts/src/HappyCorn.sol
+++ contracts/src/HappyCorn.sol
@@ -3,11 +3,13 @@
 pragma solidity 0.8.23;
 
 contract HappyCorn {
-    address public  owner;
-    address[] public sellers;
+    address private  owner;
+    address[] private sellers;
+    uint256 private immutable MAXDELIVERYTIME = 24 hours;
     
+    event CreatedItem(uint256 indexed itemid,address indexed seller);
     struct Item {
-        uint256 itemid;
+    
         string name;
         string[] imageurls;
         uint256 price;
@@ -16,17 +18,45 @@ contract HappyCorn {
         bool active;
         uint256 deliveryTime;
     }
+    mapping(uint256 => Item) public items;
+
     constructor(){
 
     }
     struct Order {
         bool deliverystatus;
+        
 
 
     }
-    function createItem() onlySeller {}
+    function createItem(string memory _name, string[] memory _imageurls, uint256 price, bool _isCustom, uint256 _deliveryTime) external {
+        require(bytes(_name).length > 0, "Name must be non-zero");
+        require(_imageurls.length > 0 && imageurls.length <=3,"Inavlid number of hashes");
+        require(_deliveryTime >= MAXDELIVERYTIME);
+        Item memory newItem = Item({name: _name,
+        imageurls: _imageurls,
+        price: _price,
+        seller: msg.sender,
+        isCustommade: _isCustom,
+        active: true,
+        deliveryTime: _deliveryTime});
+        uint256 itemid = uint256(keccak256(abi.encodePacked(_name, msg.sender, block.timestamp)));
+        items[itemid] = newItem;
+        emit CreatedItem(itemid, msg.sender);
+
+
+    }
+
+
+
+
+
+
+
 
     function removeItem() onlySeller {}
+
+    function updatelisting() onlySeller {}
 
     function updateselleraddress() onlySeller {}
 
