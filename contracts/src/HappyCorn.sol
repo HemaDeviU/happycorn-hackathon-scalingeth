971a3bc27a91e7e846eb291650b0fa7eb07d4c5a
Hema Devi <uhemadevi1927@gmail.com>
2024-04-18 19:36:01 +0530

    created escrow contract

--- contracts/src/HappyCorn.sol
+++ contracts/src/HappyCorn.sol
@@ -2,13 +2,11 @@
 
 pragma solidity 0.8.23;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract HappyCorn {
     address private owner;
-    address[] private sellers;
     uint256 constant MAXDELIVERYTIME = 24 hours;
     uint256 constant FEE_NUMERATOR = 1;
     uint256 constant FEE_DENOMINATOR = 200;
@@ -22,62 +20,67 @@ contract HappyCorn {
         address seller;
         bool isCustom;
         bool active;
-        uint160 deliveryTime;//60
+        uint160 deliveryTime;
     }
 
     mapping(uint256 => Item) public items;
     mapping(address => uint256) public balances;
 
-    event CreatedItem(uint256 indexed itemid,address indexed seller);
-    event RemovedItem(uint256 indexed itemid);
+    event CreatedItem(uint256 indexed id,address indexed seller);
+    event RemovedItem(uint256 indexed id);
     event BalanceWithdrawn(address indexed seller, uint256 indexed amount);
-    event PurchaseCompleted(itemid indexed,address indexed buyer);
+    event PurchaseCompleted(id indexed,address indexed buyer);
 
     constructor(){
+        owner = msg.sender;
     
+    }
+    function updateowner(_owner) onlyowner{
+        owner = _newowner;
     }
     
     function createItem(string memory _name, bytes32[] memory _imageurls, uint256 price, bool _isCustom, uint256 _deliveryTime) external {
         require(bytes(_name).length > 0 && price > 0 ,"Name must be non-zero");
-        require(_imageurls.length > 0 && imageurls.length <=3,"Inavlid number of hashes");
+        require(_imageurls.length > 0 && _imageurls.length <=3,"Inavlid number of hashes");
         require(_deliveryTime >= MAXDELIVERYTIME);
+        
 
         uint256 itemId = nextItemId;
         nextItemId++;
         Item memory newItem = Item({_name,_imageurls,_price,msg.sender,_isCustom,true,_deliveryTime});
-        items[itemid] = newItem;
-        emit CreatedItem(itemid, msg.sender);
+        items[id] = newItem;
+        emit CreatedItem(id, msg.sender);
     }
 
-    function removeItem(itemid) external {
-        require(items[itemid].seller == msg.sender || owner, "only seller can delete");
-        delete items[itemid];
-        emit RemovedItem(itemid);
+    function removeItem(id) external {
+        require(items[id].seller == msg.sender || owner, "only seller can delete");
+        delete items[id];
+        emit RemovedItem(id);
     }
 
     function withdrawEarnings() external {
         uint256 amount = balances[msg.sender];
         require(amount > 0), "no balance to withdraw";
         balances[msg.sender] = 0;
-        emit balancewithdrawn(msg.sender, amount);
+        emit BalanceWithdrawn(msg.sender, amount);
 
         payable(msg.sender).transfer(amount);
         
     }
     //non-custommade purchase
-    function Initiatepurchase(uint256 _itemid) payable {
-        require(msg.value >= items[itemid].price);
+    function BuyItem(uint256 _id) payable {
+        require(msg.value >= items[_id].price);
         
         uint256 feeAmount = (msg.value * FEE_NUMERATOR) / FEE_DENOMINATOR;
         balances[msg.sender] = msg.value - feeAmount;
-        emit Purchasecomplete(_itemid, msg.sender);
+        emit PurchaseCompleted(_id, msg.sender);
 
-        return items[_itemid].imageurls[2];
+        return items[_id].imageurls[2];
     }
     
 
     //custompurchase
-    function Inititatecustompurchase(_itemid)
+    function BuyCustomItem(_id)
     {//have to think about splitting the functions for ease
         //create an escrow to make sure the order is completed within deliverytime.
         //within the deliverytime, the seller must generate item's ipfs hash and update it to buyer
@@ -86,15 +89,17 @@ contract HappyCorn {
 
     }
 
-    function _refundPurchase() {}
-    function updateowner(_owner) onlyowner{
-        owner = _newowner;
-    }
+    
     //reads
-    function getOrderstatus(){}
-    function getItems() {}
-    function getseller() {}
-    function getsellerbalance() {}
+    function getItemDetails(uint256 id)public returns (string,imageurls,price,seller,isCustom,active,deliveryTime){
+        return (items[id].name,item[id].imageurls,item[id].price,item[id].price,item[id].seller,item[id].isCustom,item[id].active,item[id].delivertTime);
+    }
+    function getOrderstatus(){
+        return 
+    }
+    function getsellerbalance(address seller) {
+        return balances[seller];
+    }
 
 
 
